7200456d1e052762a6ade0c090c6ad5973ab3e6c
kyutorials <kyutorials@gmail.com>
2023-12-08 14:34:09 +0900

    feat simple seo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { DocumentHead } from "@builder.io/qwik-city";
 import Contributor from "~/components/index/contributor/contributor";
 import Title from "~/components/index/title/title";
 
@@ -6,8 +7,35 @@ import Title from "~/components/index/title/title";
 export default component$(() => {
   return (
     <>
-      <Title/>
-      <Contributor/>
+      <Title />
+      <Contributor />
     </>
   );
 });
+
+// REF : https://qwik.builder.io/docs/pages/
+export const head: DocumentHead = {
+  // This will be used to resolve the <title> of the page
+  title: 'xolving - 개발을 탐구하는 사람들의 커뮤니티',
+  meta: [
+    {
+      name: 'description',
+      content: 'xolving, 개발을 탐구하는 사람들의 커뮤니티, 디스코드. 개발자 커뮤니티입니다. 다양한 활동을 하려고 노력하며 개발자 생태계에 도움이 되는 프로젝트를 계획하고 있습니다.',
+    },
+    // Open graph
+    {
+      property: 'og:title',
+      content: 'xolving - 개발을 탐구하는 사람들의 커뮤니티',
+    },
+    {
+      property: 'og:description',
+      content: 'xolving, 개발을 탐구하는 사람들의 커뮤니티, 디스코드. 개발자 커뮤니티입니다. 다양한 활동을 하려고 노력하며 개발자 생태계에 도움이 되는 프로젝트를 계획하고 있습니다.',
+    },
+  ],
+  links: [
+    {
+      rel: 'canonical',
+      href: 'https://www.xolving.com',
+    },
+  ],
+};
