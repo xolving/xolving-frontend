a08993f6189bfb0b33611981ecb4282af1c9c743
enbraining <me@xolving.com>
2024-04-18 14:05:43 +0900

    :recycle: page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,7 @@ export default function Page(){
             <p>⚠️ 재산권 침해, 악용, 괴롭힘, 스팸, 자동 메시지 전송, 자동 전화 등의 해를 끼치는 행위를 해서는 안됩니다.</p>
             <p>👽 범죄를 계획하거나, 저지르는 등의 불법적인 행위를 해서는 안됩니다.</p>
             <p>🫥 자신의 신원을 숨기는 것은 가능하지만 왜곡하는 행위를 해서는 안됩니다.</p>
+            <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-white font-bold py-2 px-7 rounded-lg">디스코드</a>
         </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Title } from "@/components/Title";
+import DailyQ from "@/components/DailyQ";
 
 export default function Home() {
   return (
     <main>
-      <div className="text-center pt-10 font-light">
-        <Title>xolving</Title>
-        <p className="pt-3 text-xl mb-5">개발자에 의한, 개발자를 위한</p>
-        <a href="https://discord.gg/36tCw3kYuJ" className="bg-gradient-to-r from-red-500 to-orange-500 text-white font-bold py-2 px-7 rounded-lg">디스코드</a>
+      <div className="font-light">
+        <DailyQ />
       </div>
     </main>
   );
