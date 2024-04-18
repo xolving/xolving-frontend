fed8ac69aa36897826cfe7c42466b8a17c8d99f1
enbraining <me@xolving.com>
2024-04-18 14:06:07 +0900

    :fire: 공지 테두리 제거

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -5,5 +5,5 @@ interface TitleProps {
 }
 
 export const Notice: React.FC<TitleProps> = ({ children }) => {
-  return <p className="text-md border p-2 bg-red-800 text-white text-center">{children}</p>;
+  return <p className="text-md p-2 bg-red-800 text-white text-center">{children}</p>;
 }
