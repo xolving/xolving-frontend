b0dcab342945447c3a946e1a01932d1527995e24
enbraining <me@fodo.dev>
2024-07-26 19:03:22 +0900

    :recycle: 로고 색상 변경 및 헤더 설정

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,14 +3,17 @@ import HeaderIcon from "./Header-Icon";
 
 export default function Header() {
   return (
-    <header className="mt-2 px-16 flex items-center">
+    <header className="py-2 px-16 items-center grid grid-cols-3 border-b">
       <Link href={"/"}>
         <HeaderIcon width={120} height={40} />
       </Link>
-      <div className="ml-auto gap-x-8 flex text-lg font-semibold pt-2">
+      <div className="gap-x-12 flex text-lg font-semibold mx-auto">
         <Link href={"/notice"}>공지</Link>
         <Link href={"https://discord.gg/36tCw3kYuJ"}>디스코드</Link>
         <Link href={"/blog"}>블로그</Link>
+      </div>
+      <div className="text-lg font-semibold ml-auto">
+        <Link href={"/"}>대시보드</Link>
       </div>
     </header>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="text-center mt-[10rem] md:font-6xl sm:text-5xl">
-        <h1 className="font-black md:mb-3 text-orange-500">가상서버 호스팅</h1>
+        <h1 className="font-black md:mb-3 text-purple-600">가상서버 호스팅</h1>
         <p className="font-bold">현재 베타 버전입니다</p>
       </div>
     </main>
