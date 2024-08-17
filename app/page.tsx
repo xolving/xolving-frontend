76b7693ba3e4a5b21efbfe65a9abdf7e185534d2
enbraining <me@fodo.dev>
2024-08-17 23:27:27 +0900

    :recycle: 대충 목데이터

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { IcRoundKeyboardDoubleArrowDown } from '@/components/Icons';
 import styled from 'styled-components';
-import { projectList } from './mocks/project';
 
 const ScrollAnimate = styled.div`
   animation: bounce 1s infinite;
@@ -21,39 +19,73 @@ const ScrollAnimate = styled.div`
 `;
 
 export default function Home() {
+  const mocks = [
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+    {
+      title: '서버비 내기 싫어서 그냥 임시용 ㅋㅋ',
+      date: '2024년 11월 11일',
+    },
+  ];
+
   return (
-    <main className="grid">
+    <main className="grid text-neutral-200 md:mx-36 mx-4">
       <div className="left-[-70rem] top-[-40rem] w-[120rem] h-[120rem] from-[#4942ad6b] via-[#00000000] bg-gradient-radial rounded-full fixed z-[-1] bg-blend-overlay" />
       <div className="left-[30rem] top-[-5rem] w-[120rem] h-[120rem] from-[#6f42ad6b] via-[#00000000] bg-gradient-radial rounded-full fixed z-[-1] bg-blend-overlay" />
 
-      <div className="min-h-screen grid">
-        <div className="text-center mt-[10rem] md:font-6xl sm:text-5xl">
-          <div className="mb-8">
-            <h1 className="font-black md:mb-3 text-purple-600">
-              개발자 커뮤니티
-            </h1>
-            <p className="font-bold">현재 베타 버전입니다</p>
+      <div className="grid gap-y-4 mt-16">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-x-4">
+          <div className="bg-neutral-900 p-5 rounded-sm border-b">
+            <h1 className="text-lg font-semibold mb-2">공지사항</h1>
+            <div className="text-sm">
+              {mocks.slice(0, 4).map((mock) => (
+                <div key={mock.title}>
+                  <h1>{mock.title}</h1>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="mx-auto grid grid-cols-3 mt-20 w-fit gap-3">
-            {projectList.map((project) => (
-              <div
-                key={project.id}
-                className="border w-[8rem] px-5 py-3 border-neutral-600 rounded-xl"
-              >
-                <h1 className="text-lg">{project.name}</h1>
-                <p className="font-semibold text-lg">{project.content}</p>
-              </div>
-            ))}
+          <div className="bg-neutral-900 p-5 rounded-sm border-b">
+            <h1 className="text-lg font-semibold mb-2">업데이트 내역</h1>
+            <div className="text-sm">
+              {mocks.slice(0, 4).map((mock) => (
+                <div key={mock.title}>
+                  <h1>{mock.title}</h1>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
 
-        <ScrollAnimate className="mx-auto">
-          <IcRoundKeyboardDoubleArrowDown scope={35} />
-        </ScrollAnimate>
-      </div>
-
-      <div className="mx-auto font-semibold text-xl my-10">
-        <h1>하하 아무것도 없습니다.</h1>
+        <div className="bg-neutral-900 p-5 rounded-sm border-b grid">
+          <div className="text-sm grid divide-y">
+            {mocks.map((mock) => (
+              <div key={mock.title} className="py-2">
+                <h1>{mock.title}</h1>
+                <h2 className="text-neutral-300">{mock.date}</h2>
+              </div>
+            ))}
+          </div>
+          <div className="mx-auto text-lg">1 | 2 | 3 | 4 | 5 | ...</div>
+        </div>
       </div>
     </main>
   );
