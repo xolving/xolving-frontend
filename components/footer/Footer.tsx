f231d636495a813f246dab3cf0d534a6a70c7fd3
enbraining <me@fodo.dev>
2024-08-19 11:48:48 +0900

    :recycle: 모바일 사이즈 수정

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <header className="grid md:grid-cols-3 grid-cols-1 mx-36 mt-4 px-8 py-6 bg-neutral-800 rounded-sm border-t border-t-neutral-700">
+    <header className="grid gap-y-6 md:grid-cols-3 grid-cols-1 md:mx-36 mx-4 mt-4 px-8 py-6 bg-neutral-800 rounded-sm border-t border-t-neutral-700">
       <div>
         <h1 className="font-semibold text-base">팀 정보</h1>
-        <div className="grid mt-4 text-sm text-neutral-200">
+        <div className="grid md:mt-4 mt-1 text-sm text-neutral-200">
           <Link href={'/policy'}>이용약관</Link>
           <Link href={'/design'}>디자인 시스템</Link>
           <Link href={'/about'}>팀 솔빙</Link>
@@ -15,14 +15,14 @@ export function Footer() {
       </div>
       <div>
         <h1 className="font-semibold text-base">외부 링크</h1>
-        <div className="grid mt-4 text-sm text-neutral-200">
+        <div className="grid md:mt-4 mt-1 text-sm text-neutral-200">
           <Link href={'https://github.com/xolving'}>깃허브</Link>
           <Link href={'https://discord.gg/36tCw3kYuJ'}>디스코드</Link>
         </div>
       </div>
       <div>
         <h1 className="font-semibold text-base">서비스</h1>
-        <div className="grid mt-4 text-sm text-neutral-200">
+        <div className="grid md:mt-4 mt-1 text-sm text-neutral-200">
           <Link href={'https://kolosseum.fodo.dev'}>콜로세움</Link>
         </div>
       </div>
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,7 +6,7 @@ import HeaderIcon from './Header-Icon';
 
 export function Header() {
   return (
-    <header className="flex px-36 py-4 items-center">
+    <header className="flex md:mx-36 mx-4 py-4 items-center">
       <Link href={'/'}>
         <HeaderIcon width={105} height={35} />
       </Link>
@@ -15,7 +15,7 @@ export function Header() {
           href={'/login'}
           className="bg-neutral-800 rounded-lg text-sm px-3 py-1 border border-neutral-600 text-neutral-300"
         >
-          로그인 <a className="px-1">·</a> 회원가입
+          로그인 · 회원가입
         </Link>
       </div>
     </header>
